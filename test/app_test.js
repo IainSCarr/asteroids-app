41bb539d8bbe127dbd4abd693f196df0b9a8aeed
Iain <iain.carr@students.plymouth.ac.uk>
2019-12-02 12:04:11 +0000

    Server tests created

--- test/app_test.js
+++ test/app_test.js
@@ -1,2 +1,29 @@
-const assert = require('chai').assert;
-const app = require('../app.js');
+const expect = require('chai').expect;
+const request = require('request');
+
+describe('Server', function() {
+  describe('Main Page', function() {
+    it('Content', function(done) {
+        request('http://localhost:9000' , function(error, response, body) {
+            expect(body).to.equal('Hello World');
+            done();
+        });
+    });
+
+    it('Status', function(done) {
+        request('http://localhost:9000' , function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+  });
+
+  describe("Invalid Requests", function() {
+    it('Invalid Directory', function(done) {
+      request('http://localhost:9000/notreal', function(error, response, body) {
+        expect(response.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+});
