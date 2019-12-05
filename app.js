00162e8adc90dacd7c8c0f8e3987a1d825ebdc48
Iain <iain.carr@students.plymouth.ac.uk>
2019-12-05 19:48:52 +0000

    Setup adjustments

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var express = require("express");
 var app = express();
-var serv = require('http').Server(app);
-var io = require('socket.io')(serv, {});
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.use(express.static("client"));
 
 app.get('/', function (request, response) {
-  response.status(200).sendFile("/", {root: "client"});
+  response.status(200).sendFile(__dirname + '/index.html');
 });
 
-serv.listen(9000, function() {
+server.listen(9000, function() {
   console.log("Listening on 9000");
 });
 
