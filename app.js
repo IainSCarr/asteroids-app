e0948bfc083944600ae20428b84d37227c13667d
Iain <iain.carr@students.plymouth.ac.uk>
2019-12-05 19:02:41 +0000

    Socket set up

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
-
 var app = express();
+var serv = require('http').Server(app);
+var io = require('socket.io')(serv, {});
 
 app.use(express.static("client"));
 
@@ -8,6 +9,10 @@ app.get('/', function (request, response) {
   response.status(200).sendFile("/", {root: "client"});
 });
 
-app.listen(9000, function() {
+serv.listen(9000, function() {
   console.log("Listening on 9000");
+});
+
+io.sockets.on('connection', function(socket) {
+  console.log('Socket connected');
 });
