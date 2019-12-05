fe46fff563dbba8ddd24f171a7d7be3f0b9efc19
Iain <iain.carr@students.plymouth.ac.uk>
2019-12-05 22:20:14 +0000

    Multiple sockets working example

--- app.js
+++ app.js
@@ -13,6 +13,40 @@ server.listen(9000, function() {
   console.log("Listening on 9000");
 });
 
+var Socket_List = {};
+
 io.sockets.on('connection', function(socket) {
   console.log('Socket connected');
+
+  socket.id = Math.random();
+  socket.x = 0;
+  socket.y = 0;
+  socket.number = "" + Math.floor(10* Math.random());
+  Socket_List[socket.id] = socket;
+
+  socket.on('disconnect', function() {
+    delete Socket_List[socket.id];
+  });
 });
+
+setInterval(function(){
+  var pack = [];
+
+  for(var i in Socket_List) {
+    var socket = Socket_List[i];
+    socket.x++;
+    socket.y++;
+    pack.push({
+      x:socket.x,
+      y:socket.y,
+      number:socket.number
+    });
+  }
+
+  for(var i in Socket_List) {
+    var socket = Socket_List[i];
+    socket.emit('newPositions', pack);
+  }
+
+
+}, 1000/25)
