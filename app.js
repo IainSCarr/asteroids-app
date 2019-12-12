ac1540ed081451782ec009c2785d082c01b5ec17
Iain <iain.carr@students.plymouth.ac.uk>
2019-12-12 16:46:06 +0000

    Player class created and refactoring

--- app.js
+++ app.js
@@ -14,32 +14,43 @@ server.listen(9000, function() {
 });
 
 var Socket_List = {};
+var Player_List = {};
+
+class Player {
+  constructor(id) {
+    this.id = id;
+    this.x = 0;
+    this.y = 0;
+    this.number = "" + Math.floor(10* Math.random());
+  }
+}
 
 io.sockets.on('connection', function(socket) {
   console.log('Socket connected');
 
   socket.id = Math.random();
-  socket.x = 0;
-  socket.y = 0;
-  socket.number = "" + Math.floor(10* Math.random());
   Socket_List[socket.id] = socket;
+
+  var player = new Player(socket.id);
+  Player_List[socket.id] = player;
 
   socket.on('disconnect', function() {
     delete Socket_List[socket.id];
+    delete Player_List[socket.id];
   });
 });
 
 setInterval(function(){
   var pack = [];
 
-  for(var i in Socket_List) {
-    var socket = Socket_List[i];
-    socket.x++;
-    socket.y++;
+  for(var i in Player_List) {
+    var player = Player_List[i];
+    player.x++;
+    player.y++;
     pack.push({
-      x:socket.x,
-      y:socket.y,
-      number:socket.number
+      x:player.x,
+      y:player.y,
+      number:player.number
     });
   }
 
