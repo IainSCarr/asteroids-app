6ad358a7060a40e16ac1b2a4baab1fece8c7f000
Iain <iain.carr@students.plymouth.ac.uk>
2019-12-12 18:18:54 +0000

    Callback methods added

--- app.js
+++ app.js
@@ -19,9 +19,40 @@ var Player_List = {};
 class Player {
   constructor(id) {
     this.id = id;
-    this.x = 0;
-    this.y = 0;
+    this.x = 50;
+    this.y = 50;
     this.number = "" + Math.floor(10* Math.random());
+    this.pressingRight = false;
+    this.pressingLeft = false;
+    this.pressingUp = false;
+    this.isShooting = false;
+    this.maxSpeed = 10;
+    this.direction = 0;
+    this.turnSpeed = 5;
+    this.acceleration = 0.1;
+    this.velocity = [0, 0];
+  }
+
+  updatePosition() {
+    if (this.pressingRight)
+      this.direction = (this.direction + this.turnSpeed) % 360;
+    if (this.pressingLeft)
+      this.direction = (this.direction - this.turnSpeed) % 360;
+
+    console.log(this.direction);
+
+    if (this.pressingUp) {
+      this.velocity[0] += this.acceleration * Math.sin(this.direction * Math.PI / 180);
+      if (this.velocity[0] > this.maxSpeed)
+        this.velocity[0] = this.maxSpeed;
+
+      this.velocity[1] -= this.acceleration * Math.cos(this.direction * Math.PI / 180);
+      if (this.velocity[1] > this.maxSpeed)
+        this.velocity[1] = this.maxSpeed;
+    }
+
+    this.x = this.x + this.velocity[0];
+    this.y = this.y + this.velocity[1];
   }
 }
 
@@ -38,6 +69,17 @@ io.sockets.on('connection', function(socket) {
     delete Socket_List[socket.id];
     delete Player_List[socket.id];
   });
+
+  socket.on('keyPress', function(data) {
+    if (data.inputId === 'left')
+      player.pressingLeft = data.state;
+    else if (data.inputId === 'right')
+      player.pressingRight = data.state;
+    else if (data.inputId === 'up')
+      player.pressingUp = data.state;
+    else if (data.inputId === 'shoot')
+      player.isShooting = data.state;
+  });
 });
 
 setInterval(function(){
@@ -45,11 +87,11 @@ setInterval(function(){
 
   for(var i in Player_List) {
     var player = Player_List[i];
-    player.x++;
-    player.y++;
+    player.updatePosition();
     pack.push({
       x:player.x,
       y:player.y,
+      angle:player.direction,
       number:player.number
     });
   }
