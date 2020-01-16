08c1e342cb6d5bb1e9f6c914057a3aed1971bd91
Iain <iain.carr@students.plymouth.ac.uk>
2020-01-16 15:25:51 +0000

    Login template created

---
+++ client/components/login/login.js
@@ -0,0 +1,8 @@
+gameModule.component("login", {
+    templateUrl: "components/login/login.template.html",
+    controller: function GameController($scope, $http) {
+        $scope.init = function() {
+
+        }
+    }
+});
