f4389b5fd5aaa4e4bd847af95417d4fede88cd05
Iain <iain.carr@students.plymouth.ac.uk>
2020-01-17 20:04:51 +0000

    Highscore date format changed

--- client/components/scores/scores.js
+++ client/components/scores/scores.js
@@ -5,8 +5,8 @@ gameModule.component("scores", {
           $http.get("/highscores").then(function(response) {
             $("#highscores").empty().append("<thead><tr><th scope='col'>#</th><th scope='col'>Name</th><th scope='col'>Score</th><th scope='col'>Date</th></tr></thead><tbody>");
             for (var i = 0; i < response.data.length; i++) {
-              var d = new Date(response.data[i].date);
-              $("#highscores").append("<tr><th scope='row'>" + (i+1) + "</th><td>" + response.data[i].name + "</td><td>" + response.data[i].score + "</td><td>" + d.toDateString() + "</td></tr>");
+              var d = new Date(response.data[i].date).toLocaleString();
+              $("#highscores").append("<tr><th scope='row'>" + (i+1) + "</th><td>" + response.data[i].name + "</td><td>" + response.data[i].score + "</td><td>" + d + "</td></tr>");
             }
             $("#highscores").append("</tbody>");
           });
@@ -16,8 +16,8 @@ gameModule.component("scores", {
             $http.get("/highscores").then(function(response) {
               $("#highscores").empty().append("<thead><tr><th scope='col'>#</th><th scope='col'>Name</th><th scope='col'>Score</th><th scope='col'>Date</th></tr></thead><tbody>");
               for (var i = 0; i < response.data.length; i++) {
-                var d = new Date(response.data[i].date);
-                $("#highscores").append("<tr><th scope='row'>" + (i+1) + "</th><td>" + response.data[i].name + "</td><td>" + response.data[i].score + "</td><td>" + d.toDateString() + "</td></tr>");
+                var d = new Date(response.data[i].date).toLocaleString();
+                $("#highscores").append("<tr><th scope='row'>" + (i+1) + "</th><td>" + response.data[i].name + "</td><td>" + response.data[i].score + "</td><td>" + d + "</td></tr>");
               }
               $("#highscores").append("</tbody>");
           });
