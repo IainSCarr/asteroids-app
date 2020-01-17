4af98ffa28c1798a7e1f78b806b17255baf17968
Iain <iain.carr@students.plymouth.ac.uk>
2020-01-17 14:46:55 +0000

    Entity model created

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var mongoose = require("mongoose");
 var db = require("./db");
 var schemas = require("./schemas");
+var Entity = require("./models/entity");
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
@@ -57,38 +58,7 @@ server.listen(9000, function() {
 
 var Socket_List = {};
 
-class Entity {
-  constructor() {
-    this.x = Math.random() * 700;
-    this.y = Math.random() * 700;
-    this.velocity = [0, 0];
-    this.id = "";
-  }
 
-  update() {
-    this.updatePosition();
-  }
-
-  updatePosition() {
-    this.x += this.velocity[0];
-
-    if (this.x < 0)
-      this.x = 700;
-    else if (this.x > 700)
-      this.x = 0;
-
-    this.y += this.velocity[1];
-
-    if (this.y < 0)
-      this.y = 700;
-    else if (this.y > 700)
-      this.y = 0;
-  }
-
-  getDistance(entity) {
-    return Math.sqrt(Math.pow(this.x - entity.x, 2) + Math.pow(this.y - entity.y, 2));
-  }
-}
 
 class Player extends Entity {
   constructor(id) {
