dc966c2edadd9ce6550c814f90b918d20645c896
Iain <iain.carr@students.plymouth.ac.uk>
2020-01-20 18:15:36 +0000

    Comments added to client.js

--- client/client.js
+++ client/client.js
@@ -4,14 +4,14 @@ var gameModule = angular.module("asteroidsapp", ["ngRoute"]);
 var arrow_keys_handler = function(e) {
   switch(e.keyCode){
       case 37: case 39: case 38:  case 40: // Arrow keys
-      case 32:
-        if (e.target == document.body) e.preventDefault(); break; // Space
+      case 32: // Space
+        if (e.target == document.body) e.preventDefault(); break; // if focued on body prevent default behaviour
       default: break; // do not block other keys
   }
 };
 window.addEventListener("keydown", arrow_keys_handler, false);
 
-function joinGame() {
+function joinGame() { // hide and show relevant components
   $("#chatContainer").css("visibility", "visible");
   $("#loginContainer").empty();
   $("#gameContainer").css("visibility", "visible");
